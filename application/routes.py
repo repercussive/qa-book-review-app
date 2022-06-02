415cac25e471bff21ab765e36ae4e55046ddb121
= <liam2robertson@gmail.com>
2022-06-02 13:46:51 +0100

    add index.html

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,10 @@
 from application import app
+from flask import render_template
 
 @app.route('/')
 def home():
-  return "There's nothing here yet!"
+  return render_template('index.html')
+
+@app.route('/add-book', methods=['GET', 'POST'])
+def add_book():
+  return 'not yet implemented 🤷‍♂️'
