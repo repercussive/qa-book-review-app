e21e11ed105bde3b0d5105f6b58483b04d407daf
= <liam2robertson@gmail.com>
2022-06-02 14:07:58 +0100

    create form for adding new books

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,8 @@
-from application import app
-from flask import render_template
+from application import app, db
+from application.forms import NewBookForm
+from application.models import Book
+from flask import render_template, redirect, url_for
+
 
 @app.route('/')
 def home():
@@ -7,4 +10,12 @@ def home():
 
 @app.route('/add-book', methods=['GET', 'POST'])
 def add_book():
-  return 'not yet implemented 🤷‍♂️'
+  form = NewBookForm()
+
+  if form.validate_on_submit():
+    new_book = Book(title=form.title.data, author=form.author.data)
+    db.session.add(new_book)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+  return render_template('add-book.html', form=form)
