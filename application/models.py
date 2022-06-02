dbeef8fa21186f645cf5fc7f20c184fb286541b8
= <liam2robertson@gmail.com>
2022-06-02 17:46:25 +0100

    add review model

--- application/models.py
+++ application/models.py
@@ -5,3 +5,12 @@ class Book(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(60))
   author = db.Column(db.String(40))
+  reviews = db.relationship('Review', backref='book')
+
+
+class Review(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  rating = db.Column(db.Integer)
+  headline = db.Column(db.String(100))
+  body = db.Column(db.String(8000))
+  book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
