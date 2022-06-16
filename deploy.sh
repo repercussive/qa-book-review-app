48f2ea945c7b01ade7daaa7956f2b3a22e20da71
= <liam2robertson@gmail.com>
2022-06-16 12:17:19 +0100

    add echo statements to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+echo "--- Preparing to run app ---"
 sudo apt update
 sudo apt install python3 python3-pip python3-venv -y
+
+echo "--- Activating venv ---"
 python3 -m venv venv
 source venv/bin/activate
+
+echo "--- Installing dependencies ---"
 pip3 install -r requirements.txt
+
+echo "--- Killing existing processes ---"
 kill $(cat gunicornpidfile)
+
+echo "--- Running app ---"
 python3 -m gunicorn -D --workers 4 --bind 0.0.0.0:5000 app:app -p gunicornpidfile
